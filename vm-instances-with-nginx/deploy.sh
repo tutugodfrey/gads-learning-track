29acb19bf17b013dc217a78fbe752f8c2ffffa39
Godfrey Tutu <godfreytutu@Godfreys-MacBook-Pro.local>
2020-09-02 13:11:22 +0100

    updata metadata-flag: add startup-script key

--- vm-instances-with-nginx/deploy.sh
+++ vm-instances-with-nginx/deploy.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Create Firewall rule
+gcloud compute --project=$DEVSHELL_PROJECT_ID firewall-rules create default-allow-http \
+--direction=INGRESS --priority=1000 --network=default --action=ALLOW \
+--rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
+
 # Create Vm 1
 gcloud compute instances create my-vm-1 \
 --zone us-central1-a \
@@ -7,7 +12,7 @@ gcloud compute instances create my-vm-1 \
 --image-project "debian-cloud" \
 --image "debian-9-stretch-v20190213" \
 --tags "http-server" \
---metadata-from-file "./install-nginx.sh" \
+--metadata-from-file startup-script="./install-nginx.sh" \
 --subnet "default" 
 
 # Create vm 2 
@@ -17,5 +22,5 @@ gcloud compute instances create my-vm-2 \
 --image-project "debian-cloud" \
 --image "debian-9-stretch-v20190213" \
 --tags "http-server" \
---metadata-from-file "./install-nginx.sh" \
+--metadata-from-file startup-script="./install-nginx.sh" \
 --subnet "default"
--- vm-instances-with-nginx/install-nginx.sh
+++ vm-instances-with-nginx/install-nginx.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
 
 sudo apt-get install nginx-light -y
-echo "Hi from Google Cloud Expert" > /var/www/html/index.nginx-debian.html
+echo "<htm>
+  <head>
+    <title>My Nginx server</title>
+  </head>
+  <body>
+    <h1>Nginx Server</h1>
+    <p>
+      Hi from Google Cloud Expert
+    </p>
+  </body>
+</html>
+" > /var/www/html/index.nginx-debian.html
