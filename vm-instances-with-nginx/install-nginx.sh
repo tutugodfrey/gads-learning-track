5661535366cd16a0070674bacad28be85de51ace
tutugodfrey <godfrey_tutu@yahoo.com>
2020-09-02 19:24:11 +0100

    add line apt-get update

--- vm-instances-with-nginx/install-nginx.sh
+++ vm-instances-with-nginx/install-nginx.sh
@@ -1,15 +1,5 @@
 #! /bin/bash
 
+sudo apt-get update
 sudo apt-get install nginx-light -y
-echo "<htm>
-  <head>
-    <title>My Nginx server</title>
-  </head>
-  <body>
-    <h1>Nginx Server</h1>
-    <p>
-      Hi from Google Cloud Expert
-    </p>
-  </body>
-</html>
-" > /var/www/html/index.nginx-debian.html
+sudo mv index.html > /var/www/html/index.nginx-debian.html
