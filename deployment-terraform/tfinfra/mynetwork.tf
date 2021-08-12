7cb2086706a66321dd3a52eadb1208a64afaef2e
edXProjectUser-at-498289857405 <godfrey_tutu@yahoo.com>
2021-08-12 10:05:07 +0000

    add terraform deployment option

--- deployment-terraform/tfinfra/mynetwork.tf
+++ deployment-terraform/tfinfra/mynetwork.tf
@@ -1,17 +1,17 @@
 # Create the mynetwork network
 
 resource "google_compute_network" "mynetwork" {
-  name = "mynetwork"
+  name                    = "mynetwork"
   auto_create_subnetworks = true
 }
 
 # Add a firewall rule to allow http, ssh, RDP and ICMP traffic on mynetwork
 resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
-  name = "mynetwork-allow-http-ssh-rdp-icmp"
+  name    = "mynetwork-allow-http-ssh-rdp-icmp"
   network = google_compute_network.mynetwork.self_link
   allow {
     protocol = "tcp"
-    ports = ["22", "80", "3389"]
+    ports    = ["22", "80", "3389"]
   }
   allow {
     protocol = "icmp"
@@ -20,16 +20,16 @@ resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
 
 # Create the mynet-us-vm instance
 module "mynet-us-vm" {
-  source = "./instance"
-  instance_name = "mynet-us-vm"
-  instance_zone = "us-central1-a"
+  source           = "./instance"
+  instance_name    = "mynet-us-vm"
+  instance_zone    = "us-central1-a"
   instance_network = google_compute_network.mynetwork.self_link
 }
 
 # Create the mynet-eu-vm instance
 module "mynet-eu-vm" {
-  source = "./instance"
-  instance_name = "mynet-eu-vm"
-  instance_zone = "europe-west1-d"
+  source           = "./instance"
+  instance_name    = "mynet-eu-vm"
+  instance_zone    = "europe-west1-d"
   instance_network = google_compute_network.mynetwork.self_link
 }
